83fcb971e0b20dfc70c4f79723a64c4d619883ad
派赛克 <guowenlong20000@sina.com>
2017-02-26 01:41:26 +0800

    0.0.7 修改BaseFragment为V4包的fragment

--- mix-utils/src/main/java/me/wenlong/mixutils/mvp/BaseFragment.java
+++ mix-utils/src/main/java/me/wenlong/mixutils/mvp/BaseFragment.java
@@ -1,15 +1,17 @@
 package me.wenlong.mixutils.mvp;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.HashMap;
+
 import butterknife.ButterKnife;
-import me.wenlong.mixutils.MixUtils;
 
 /**
  * des   : Fragment基类
@@ -17,33 +19,59 @@ import me.wenlong.mixutils.MixUtils;
  * email : guowenlong20000@gmail.com
  * time  : 2016年05月31日 上午 11:32.
  */
-public abstract class BaseFragment  extends Fragment {
+public abstract class BaseFragment extends Fragment {
 
     public View view;
     public Context mContext;
-    public SharedPreferences sp;
+    protected SharedPreferences sp;
+    protected HashMap<String, Object> mFromMap;
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (!hidden)initView();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = initContantView(inflater);
         ButterKnife.bind(this, view);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
         return view;
+    }
+
+    public void setFromMap(HashMap<String, Object> map) {
+        this.mFromMap = map;
+        initView();
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         mContext = getActivity();
-        sp = MixUtils.mSp;
+        sp = mContext.getSharedPreferences("config", mContext.MODE_PRIVATE);
         initView();
         initData(savedInstanceState);
         super.onActivityCreated(savedInstanceState);
     }
 
-
     public abstract void initData(Bundle savedInstanceState);
 
-    public abstract void initView();//创建和非隐藏状态时候都会运行此方法
+    public abstract void initView();
 
     public abstract View initContantView(LayoutInflater inflater);
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 }
