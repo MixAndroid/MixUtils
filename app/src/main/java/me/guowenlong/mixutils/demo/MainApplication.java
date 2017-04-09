88063059de6790565fc2d15ccfa0dfb9b2ff85e0
派赛克 <guowenlong20000@sina.com>
2017-04-09 23:15:00 +0800

    2017.3.28 0.1.3 增加LeakCanary

--- app/src/main/java/me/guowenlong/mixutils/demo/MainApplication.java
+++ app/src/main/java/me/guowenlong/mixutils/demo/MainApplication.java
@@ -1,7 +1,7 @@
 package me.guowenlong.mixutils.demo;
 
 import android.app.Application;
-import com.squareup.leakcanary.LeakCanary;
+import me.wenlong.mixutils.MixUtils;
 
 /**
  * Created by hz51390 on 2017/3/7.
@@ -12,11 +12,6 @@ public class MainApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
-    if (LeakCanary.isInAnalyzerProcess(this)) {
-      // This process is dedicated to LeakCanary for heap analysis.
-      // You should not init your app in this process.
-      return;
-    }
-    LeakCanary.install(this);
+    MixUtils.install(this);
   }
 }
--- mix-utils/src/main/java/me/wenlong/mixutils/MixUtils.java
+++ mix-utils/src/main/java/me/wenlong/mixutils/MixUtils.java
@@ -1,11 +1,11 @@
 package me.wenlong.mixutils;
 
+import android.app.Application;
 import android.content.Context;
-
 import com.elvishew.xlog.LogLevel;
 import com.elvishew.xlog.XLog;
 import com.facebook.drawee.backends.pipeline.Fresco;
-
+import com.squareup.leakcanary.LeakCanary;
 import me.wenlong.mixutils.util.LogUtils;
 import me.wenlong.mixutils.util.SpUtil;
 import wenlong.me.mixutils.BuildConfig;
@@ -18,11 +18,15 @@ public class MixUtils {
 
   public static Context mContext;
 
-  public static void install(Context context) {
-    mContext = context;
-    Fresco.initialize(context);
+  public static void install(Application application) {
+    mContext = application.getApplicationContext();
+    Fresco.initialize(mContext);
     LogUtils.allowLog = !BuildConfig.DEBUG;
     XLog.init(BuildConfig.DEBUG ? LogLevel.ALL : LogLevel.NONE);
     SpUtil.init(mContext);
+    if (LeakCanary.isInAnalyzerProcess(mContext)) {
+      return;
+    }
+    LeakCanary.install(application);
   }
 }
